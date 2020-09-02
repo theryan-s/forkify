50c912e5d31a63aa2dc8a2cf1ac37decc5eb1d10
Ryan Shao <ryan.shao95@gmail.com>
2020-09-02 17:59:56 -0400

    search view added

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global State of the app
  * Search Object
@@ -9,21 +11,42 @@ import Search from './models/Search';
 
 const state = {};
 
-const controlSearch = () => {
+const controlSearch = async () => {
     // 1. Get query from view
-    const query = 'pizza' 
+    const query = searchView.getInput(); 
 
     if (query) {
-        
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        //5. Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.results);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e =>{
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     controlSearch();
-})
+});
 
-const search = new Search('pizza');
-console.log(search);
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn){
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.results, goToPage);
+    }
+});
 
-search.getResults();
+// const search = new Search('pizza');
+// console.log(search);
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,8 +7,8 @@ export default class Search {
 
     async getResults(){
         try {
-            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
-            this.result = res.data.recipes;
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.results = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
             alert(error);
