91c0733d756656d2f43f56f4521c7cca9989a13a
Ryan Shao <ryan.shao95@gmail.com>
2020-09-02 10:21:14 -0400

    finished setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-// Global app controller
+import Search from './models/Search';
+
+/** Global State of the app
+ * Search Object
+ * Current Recipe Object
+ * Shopping list object
+ * Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = () => {
+    // 1. Get query from view
+    const query = 'pizza' 
+
+    if (query) {
+        
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+})
+
+const search = new Search('pizza');
+console.log(search);
+
+search.getResults();
